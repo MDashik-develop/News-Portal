f060c8252bd811020e289974283cb01617247294
MDashik-develop <MDashik.design.laptop@gmail.com>
2025-06-30 03:41:17 +0600

    Remove 'role' column from User model, migration, and factory; add unique username generation in UserFactory.

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'username',
         'email',
         'password',
-        'role',
+        // 'role',
         'profile_image',
         'bio',
         'status',
