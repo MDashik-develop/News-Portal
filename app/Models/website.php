fb113c234ced1c8d0fcb649a3dc351089fc57ec2
MDashik-develop <MDashik.design.laptop@gmail.com>
2025-06-30 12:44:00 +0600

    Add HasFactory trait to website model and clean up ExampleTest by removing unnecessary website record creation

--- app/Models/website.php
+++ app/Models/website.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class website extends Model
 {
     //
+    use HasFactory;
 protected $fillable = [
     'title',
     'favicon',
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,18 +1,6 @@
 <?php
 
-use App\Models\Website;
-
 test('returns a successful response', function () {
-    // Ensure there's a Website record so views don't error
-    Website::factory()->create([
-        'title' => 'Test Site',
-        'meta_description' => 'Test Description',
-        'meta_tags' => 'test,site',
-        'logo' => 'website/defaultlogo.png',
-        'favicon' => 'website/defaultfav.png',
-        'fb_app_id' => '1234567890',
-    ]);
-
     $response = $this->get('/');
 
     $response->assertStatus(200);
